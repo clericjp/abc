bc3cb1d6af74b32506bea55938f4495323f37b0d
ruslan.safin <safin_ruslan_a@mail.ru>
2017-09-22 20:20:38 +0300

    Initial commit

---
+++ 03/Practice_03-03/src/friday/User.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package friday;
+
+/**
+ *
+ * @author ruslan.safin
+ */
+public class User {
+    
+    
+    public static void main (String [] args)    {
+        String username = "Ruslan";
+        System.out.println("Hello "+ username);
+    }
+}
